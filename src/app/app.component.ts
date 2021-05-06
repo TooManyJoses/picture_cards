35cdab0761ad5317b1a4e41e16bb4001a96203c1
Iron.Snowflake <jadrubio@gmail.com>
2021-05-05 22:59:48 -0400

    looping through array and passng info to card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts =[
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'hiking4nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snow Mountain',
+      imageUrl: 'assets/mountain.jpg',
+      username: 'snowshoerunner',
+      content: 'Lovely day snowshoeing in Estes Park'
+    },
+    {
+      title: 'Sandy Beach',
+      imageUrl: 'assets/beach.jpg',
+      username: 'fun-in-the-sun',
+      content: 'Counting down the days until I am back!'
+    },
+  ]
 }
